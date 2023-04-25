25e036f7238ab4e835b5aa4ac2ad8bfa66d9ac9e
Brave N <dung.nguyentuan@adamosoft.com>
2023-04-25 14:28:55 +0700

    ok

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const About = () => {
+interface DataSer {
+  data: [];
+}
+interface item {
+  albumId:number,
+  id:number,
+  title:string,
+  url:string
+}
+const About = ({data}:DataSer) => {
   return (
+    <>
     <div>about</div>
+    <div>{data.length}</div>
+    </>
   )
 }
 export default About
+export async function getServerSideProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+  const data = await res.json();
+  return { props: { data } };
+}
